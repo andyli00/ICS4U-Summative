d9583f790567b88bf8b234fb089e74e3a8bcac90
Andy Li <landy1237@gmail.com>
2017-12-04 17:54:41 -0500

    Merge branch 'master' of https://github.com/blechd/ICS4U-Summative

--- src/main/CommonManagement.java
+++ src/main/CommonManagement.java
@@ -1,13 +1,29 @@
 package main;
 
-
 import features.NoteManager;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 /**
  * GUI for the main application
  * @author Andy Li
  * @since Nov 30, 2017
  */
-public class CommonManagement {
-	//asdf
+public class CommonManagement extends Application {
+	
+	ProjectManager projectManager;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setScene(new Scene(new StackPane()));
+		//primaryStage.show();
+		new NoteManager();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
 }
---
+++ src/main/ProjectManager.java
@@ -0,0 +1,16 @@
+package main;
+
+import organization.Project;
+import organization.TodoList;
+import organization.Task;
+
+import javafx.scene.layout.BorderPane;
+
+/**
+ * A manager class for projects, todolists, and tasks
+ * @author Andy Li
+ * @since Dec 04, 2017
+ */
+public class ProjectManager {
+
+}
